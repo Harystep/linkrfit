9f6df1e72887096fe29d33fc36040457898a7044
Harystep <warm588@163.com>
2023-04-04 19:43:55 +0800

    test .gitignore

---
+++ PowerDance/Module/Train/TrainAction/Model/ZCTrainActionBaseModel.h
@@ -0,0 +1,16 @@
+//
+//  ZCTrainActionBaseModel.h
+//  PowerDance
+//
+//  Created by oneStep on 2023/4/4.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZCTrainActionBaseModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PowerDance/Module/Train/TrainAction/Model/ZCTrainActionBaseModel.m
@@ -0,0 +1,12 @@
+//
+//  ZCTrainActionBaseModel.m
+//  PowerDance
+//
+//  Created by oneStep on 2023/4/4.
+//
+
+#import "ZCTrainActionBaseModel.h"
+
+@implementation ZCTrainActionBaseModel
+
+@end
